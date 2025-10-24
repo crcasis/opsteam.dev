add9cba4719cda2b14ac59cf6db8a179576c93a9
Cristian Casis <crcasis@gmail.com>
2025-10-24 13:29:31 +0200

    update icon

--- src/components/layout/TopBar/index.tsx
+++ src/components/layout/TopBar/index.tsx
@@ -36,17 +36,41 @@ const Header = ({ theme, classname, user, buynow, searchbar, userSignIn, showCar
       <header data-bs-theme={`${theme}`}>
         <div className={`navbar navbar-expand-lg fixed-top ${classname} navbar-${scrollY > 100 && 'stuck'}`}>
           <Container>
-            <Link className="navbar-brand pe-sm-3" to="/">
-              <span className="text-primary flex-shrink-0 me-2">
-                <svg width={35} height={32} viewBox="0 0 36 33" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    fill="currentColor"
-                    d="M19.14,12.94a7.49,7.49,0,0,0,0-1.88l2.11-1.65a.5.5,0,0,0,.12-.64l-2-3.46a.5.5,0,0,0-.6-.22l-2.49,1a7.42,7.42,0,0,0-1.62-.94l-.38-2.65A.5.5,0,0,0,13.7,2H10.3a.5.5,0,0,0-.49.41L9.43,5.06a7.42,7.42,0,0,0-1.62.94l-2.49-1a.5.5,0,0,0-.6.22l-2,3.46a.5.5,0,0,0,.12.64L4.86,11.06a7.49,7.49,0,0,0,0,1.88L2.75,14.59a.5.5,0,0,0-.12.64l2,3.46a.5.5,0,0,0,.6.22l2.49-1a7.42,7.42,0,0,0,1.62.94l.38,2.65A.5.5,0,0,0,10.3,22h3.4a.5.5,0,0,0,.49-.41l.38-2.65a7.42,7.42,0,0,0,1.62-.94l2.49,1a.5.5,0,0,0,.6-.22l2-3.46a.5.5,0,0,0-.12-.64ZM12,15.5A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"
-                  ></path>
-                </svg>
-              </span>
-              Opsteam
-            </Link>
+          <Link className="navbar-brand pe-sm-3" to="/">
+  <span className="text-primary flex-shrink-0 me-2">
+    <svg
+      version="1.1"
+      id="Capa_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      viewBox="0 0 57 57"
+      style={{ enableBackground: 'new 0 0 57 57' }}
+      xmlSpace="preserve"
+      width={35}
+      height={35}
+    >
+      <rect x="0" y="0" width="57" height="57" fill="#ECF0F1" />
+      <path
+        fill="#BDC3C7"
+        d="M20,44c-0.256,0-0.512-0.098-0.707-0.293L4.586,29l14.707-14.707c0.391-0.391,1.023-0.391,1.414,0
+        s0.391,1.023,0,1.414L7.414,29l13.293,13.293c0.391,0.391,0.391,1.023,0,1.414C20.512,43.902,20.256,44,20,44z"
+      />
+      <path
+        fill="#BDC3C7"
+        d="M37,44c-0.256,0-0.512-0.098-0.707-0.293c-0.391-0.391-0.391-1.023,0-1.414L49.586,29L36.293,15.707
+        c-0.391-0.391-0.391-1.023,0-1.414s1.023-0.391,1.414,0L52.414,29L37.707,43.707C37.512,43.902,37.256,44,37,44z"
+      />
+      <path
+        fill="#BDC3C7"
+        d="M24.001,50c-0.071,0-0.144-0.008-0.216-0.023c-0.539-0.118-0.88-0.651-0.762-1.191l9-41
+        c0.118-0.539,0.654-0.88,1.191-0.762c0.539,0.118,0.88,0.651,0.762,1.191l-9,41C24.874,49.682,24.46,50,24.001,50z"
+      />
+    </svg>
+  </span>
+  Opsteam
+</Link>
 
             <ThemeMode />
 
