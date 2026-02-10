edb10ae4389f1085dfdb28b17a8c979134077316
Cristian <cristiandevoteam@Cristians-MacBook-Air.local>
2026-02-10 10:55:08 +0000

    update partner

--- src/pages/landing/opsteam/components/Award.tsx
+++ src/pages/landing/opsteam/components/Award.tsx
@@ -10,14 +10,14 @@ const Award = () => {
         <Row className="justify-content-center row-cols-3 gx-0 gx-sm-3 gx-md-2 gx-lg-3 gx-xl-4">
           <Col>
             <div className="py-sm-2 my-xl-1">
-              <Image className="d-block d-dark-mode-none mx-auto" src={linux} width={196} alt="Linux" />
-              <Image className="d-none d-dark-mode-block mx-auto" src={linux} width={196} alt="Linux" />
+              <Image className="d-block d-dark-mode-none mx-auto" src={gcpPartner} width={196} alt="GCP Partner" />
+              <Image className="d-none d-dark-mode-block mx-auto" src={gcpPartner} width={196} alt="GCP Partner" />
             </div>
           </Col>
           <Col>
             <div className="py-sm-2 my-xl-1">
-              <Image className="d-block d-dark-mode-none mx-auto" src={gcpPartner} width={196} alt="GCP Partner" />
-              <Image className="d-none d-dark-mode-block mx-auto" src={gcpPartner} width={196} alt="GCP Partner" />
+              <Image className="d-block d-dark-mode-none mx-auto" src={linux} width={196} alt="Linux" />
+              <Image className="d-none d-dark-mode-block mx-auto" src={linux} width={196} alt="Linux" />
             </div>
           </Col>
           <Col>
